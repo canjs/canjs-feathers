d6e18e46f44ebdc7c4cf47536839e996517465a4
David Luecke <daff@neyeon.com>
2014-04-09 23:52:01 -0600

    Initial commit for setup and test harness.

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/* global describe, it, window, require */
+(function () {
+  var assert = window.assert;
+
+  describe('can.Feathers.Model', function () {
+    it('does something', function () {
+      assert.ok(can.Feathers, 'Did something');
+    });
+  });
+})();
