7c5419ebc90dfed812defd5679d89b93faafd8ed
David Luecke <daff@neyeon.com>
2014-04-10 21:41:48 -0600

    Finalizing functionality.

--- test/test.js
+++ test/test.js
@@ -3,6 +3,15 @@
   var assert = window.assert;
 
   describe('can.Feathers.Model', function () {
+    var Todo;
+
+    before(function(done) {
+      can.Feathers.connect().then(function() {
+        Todo = can.Feathers.model('/todos/');
+        done();
+      });
+    });
+
     it('initializes can.Feathers', function () {
       assert.ok(can.Feathers, 'can.Feathers exists');
     });
@@ -28,43 +37,43 @@
         url: '/todos/clear',
         dataType: 'json'
       }).then(function () {
-          // ::create
-          laundry.save().then(function (todo) {
-            assert.deepEqual(todo.attr(), {
-              id: 0,
-              description: 'Do some laundry!',
-              done: false
-            });
+        // ::create
+        laundry.save().then(function (todo) {
+          assert.deepEqual(todo.attr(), {
+            id: 0,
+            description: 'Do some laundry!',
+            done: false
+          });
 
-            todo.attr('description', 'Really do laundry!');
+          todo.attr('description', 'Really do laundry!');
 
-            // ::update
-            return todo.save();
-          })
-            .then(function (todo) {
-              // ::find
-              return Todo.findAll();
-            })
-            .then(function (todos) {
-              assert.equal(todos.length, 1, 'Got one todo');
-              assert.deepEqual(todos[0].attr(), expectedLatest,
-                'findAll returned with updated Todo');
-              // ::get
-              return Todo.findOne({ id: todos[0].id });
-            })
-            .then(function (todo) {
-              assert.deepEqual(todo.attr(), expectedLatest, 'findOne returned');
-              // ::remove
-              return todo.destroy();
-            })
-            .then(function () {
-              return Todo.findAll();
-            })
-            .then(function (todos) {
-              assert.equal(todos.length, 0, 'Deleted todo');
-              done();
-            });
+          // ::update
+          return todo.save();
+        })
+        .then(function (todo) {
+          // ::find
+          return Todo.findAll();
+        })
+        .then(function (todos) {
+          assert.equal(todos.length, 1, 'Got one todo');
+          assert.deepEqual(todos[0].attr(), expectedLatest,
+            'findAll returned with updated Todo');
+          // ::get
+          return Todo.findOne({ id: todos[0].id });
+        })
+        .then(function (todo) {
+          assert.deepEqual(todo.attr(), expectedLatest, 'findOne returned');
+          // ::remove
+          return todo.destroy();
+        })
+        .then(function () {
+          return Todo.findAll();
+        })
+        .then(function (todos) {
+          assert.equal(todos.length, 0, 'Deleted todo');
+          done();
         });
+      });
     });
 
     it('passes service errors', function (done) {
@@ -77,10 +86,9 @@
     });
 
     it('creating model via REST send created event', function (done) {
-      var Todo = can.Feathers.model('/todos/');
-
       Todo.bind('created', function (ev, todo) {
         assert.equal(todo.attr('description'), 'Some test');
+        Todo.unbind('created');
         done();
       });
 
@@ -91,7 +99,7 @@
       });
     });
 
-    it('updating model via REST updates live binding', function (done) {
+    it('updating model via REST updates DOM', function (done) {
       var Todo = can.Feathers.model('/todos/');
       var ta = $('#testarea');
 
@@ -100,6 +108,7 @@
         assert.equal(ta.find('p.done').html(), 'Live-bound',
           'Live bound template updated with REST data');
         ta.empty();
+        Todo.unbind('updated');
         done();
       });
 
@@ -118,8 +127,31 @@
       });
     });
 
-    it.skip('removing a model via REST removes entry from live binding', function(done) {
+    it('removing a model via REST removes entry from DOM', function(done) {
+      var Todo = can.Feathers.model('/todos/');
+      var ta = $('#testarea');
 
+      Todo.bind('destroyed', function () {
+        assert.equal(ta.find('div').html(), '', 'Div is now empty');
+        ta.empty();
+        Todo.unbind('destroyed');
+        done();
+      });
+
+      new Todo({
+        description: 'remove test'
+      }).save().then(function (todo) {
+        var renderer = can.view.mustache('<div>{{#each list}}' +
+          '<p class="{{^if done}}todo{{/if}}">{{description}}</p>' +
+          '{{/each}}</div>');
+        ta.append(renderer({ list: new Todo.List([todo]) }));
+        assert.equal(ta.find('p.todo').length, 1, 'One todo rendered in list');
+
+        can.ajax({
+          url: '/todos/' + todo.id,
+          method: 'DELETE'
+        });
+      });
     });
   });
 })();
